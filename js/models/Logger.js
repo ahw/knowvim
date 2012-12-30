0bd68980eddf3db5c9d976bccacee837a29b7662
ahw <andrewhallagan@gmail.com>
2012-12-30 17:52:39 -0500

    Better logging.
    Added ability to log from all modules using "log=all" in the query string.
    Also can control the logging threshold with the "level" query parameter.
    Supported levels are
    
        level=debug
        level=info
        level=warn

--- js/models/Logger.js
+++ js/models/Logger.js
@@ -10,12 +10,33 @@ var Logger = function(args) {
     this.prefix = args.prefix + ': ';
 
     this.isEnabled = function() {
-        var re = new RegExp("\\?log=[a-zA-Z0-9;]*" + this.module);
+        var re = new RegExp("\\?log=[a-zA-Z0-9;]*(" + this.module + "|all)");
         return re.test(window.location.search);
     };
 
+    this.meetsThreshold = function(inputLevel) {
+        var thresholdLevel = this.getLevel();
+        return this.levels[inputLevel] >= this.levels[thresholdLevel];
+    };
+
+    this.getLevel = function() {
+        var matches = window.location.search.match(/level=(debug|info|warn|error)/);
+        if (matches) {
+            return matches[1];
+        } else {
+            return 'debug';
+        }
+    };
+
+    this.levels = {
+        debug : 0,
+        info : 1,
+        warn : 2,
+        error : 3
+    };
+
     this.log = function(msg, obj) {
-        if (!this.isEnabled())
+        if (!this.isEnabled() || !this.meetsThreshold('info'))
             return;
 
         if (obj)
@@ -25,7 +46,7 @@ var Logger = function(args) {
     };
 
     this.warn = function(msg, obj) {
-        if (!this.isEnabled())
+        if (!this.isEnabled() || !this.meetsThreshold('warn'))
             return;
 
         if (obj)
