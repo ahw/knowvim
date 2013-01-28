3f5b48425c9151b5c9bb7a90908264e4aa323c48
ahw <andrewhallagan@gmail.com>
2013-01-27 23:26:52 -0800

    Can't help myself; deleting old stuff.
    Not that they're harming anything, but deleting a bunch of old crusty files.

--- instruction-panel.php
+++
@@ -1,5 +0,0 @@
-<div id="instruction_panel" class="panel mainbox">
-<a class="panel_link" id="instruction_panel_hide_link" href="#">Hide instruction panel.</a><br/>
-Here is the instruction panel.
-</div>
-
