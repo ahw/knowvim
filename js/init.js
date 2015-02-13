cefd4815f5ac6de45bb5ce7ab786a45b8b7a5bfc
ahw <andrewhallagan@gmail.com>
2015-02-12 19:39:02 -0800

    Source un-built files
    Replaced CoffeeScript files with JavaScript equivalents. index.html sources
    un-built files so they'll work hopefully work on https://ahw.github.io/knowvim

--- js/init.js
+++ js/init.js
@@ -1,33 +1,30 @@
-$(document).ready(function() {
-
-    vim = new Vim();
-    editor = new EditorView({model : vim});
-    app = new ApplicationView({editorView : editor});
-    var matches = window.location.search.match(/file=([^&]+)/);
-    var fileName = matches ? matches[1] : 'grammar.txt';
-    vim.openBuffer(fileName, function() {
-        var macroString = Macros.getQueryStringMacro();
-        if (typeof macroString == 'string') {
-            Macros.runMacroString({
-                vim : vim,
-                macroString : macroString
-            });
-        }
-    });
-    toastr.options = {
-      "closeButton": false,
-      "debug": false,
-      "progressBar": false,
-      "positionClass": "toast-top-center",
-      "onclick": null,
-      "showDuration": "300",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut",
-      "preventDuplicates": true
-    };
+vim = new Vim();
+editor = new EditorView({model : vim});
+app = new ApplicationView({editorView : editor});
+var matches = window.location.search.match(/file=([^&]+)/);
+var fileName = matches ? matches[1] : 'grammar.txt';
+vim.openBuffer(fileName, function() {
+    var macroString = Macros.getQueryStringMacro();
+    if (typeof macroString == 'string') {
+        Macros.runMacroString({
+            vim : vim,
+            macroString : macroString
+        });
+    }
 });
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "progressBar": false,
+  "positionClass": "toast-top-center",
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut",
+  "preventDuplicates": true
+};
--- syntax-highlighter.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript" src="/js/shCore.js"></script>
-<script type="text/javascript" src="/js/shBrushJScript.js"></script>
-<script type="text/javascript" src="/js/shBrushXml.js"></script>
-<script type="text/javascript" src="/js/shBrushPlain.js"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-    SyntaxHighlighter.all();
-});
-</script>
